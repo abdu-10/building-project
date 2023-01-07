47b36ffd625d76f2c9eb22e7dae75d30d8ccb04a
abdulrahman <abdulrahman.omar@student.moringschool.com>
2023-01-07 11:27:42 +0300

    adds scripts

---
+++ assets/scripts/login.js
@@ -0,0 +1,30 @@
+const trysession = document.getElementById("createacc");
+let loggingInUser;
+
+function handleLogin(e) {
+    e.preventDefault();
+
+    let logInData = {
+        name: document.getElementById("uname").value,
+        password: document.getElementById("upass").value,
+    };
+    loggingInUser = logInData;
+    fetch("https://data-store-server-production.up.railway.app/users/")
+    .then(r => r.json())
+    .then(userValidator)
+}
+function userValidator(users){
+    users.forEach(showUser);
+}
+function showUser(object){
+    const attemptlog = []
+    attemptlog.push(object.username)
+    attemptlog.push(object.password)
+    if (attemptlog[0] == loggingInUser.name && attemptlog[1] == loggingInUser.password){
+        window.location = "../../static/home.html"
+    }else{
+        console.log("invalid username or password")
+    }
+    trysession.reset()
+}
+trysession.addEventListener("submit", handleLogin)
---
+++ assets/scripts/signup.js
@@ -0,0 +1,32 @@
+const createuser = document.getElementById('createacc');
+
+createuser.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {   
+    e.preventDefault();
+    let userData = {
+        username: document.getElementById("uname").value,
+        image: document.getElementById("uimage").value,
+        city: document.getElementById("ucity").value,
+        password: document.getElementById("upass").value,
+    };
+    fetch("https://data-store-server-production.up.railway.app/users", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+    .then(r => r.json())
+    .then( (userObject) => {
+        if (userObject !== {} || ""){
+            //replace here with autologin
+            window.location = "../../index.html";
+            alert("Account created successfully, proceed to log in");
+        }else{
+            console.log("Error creating account")
+            // this will be replaced by a prompt for the user to retry
+        }
+    });
+    createuser.reset("")
+}
